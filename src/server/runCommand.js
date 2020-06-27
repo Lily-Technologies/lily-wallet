ef7c4094ea7369e4ea3ad9c112651ae47c9fe3f0
Leonardo <leo.jobim@gmail.com>
2020-06-27 16:03:21 -0300

    Add hwi for Linux

--- src/server/runCommand.js
+++ src/server/runCommand.js
@@ -1,19 +1,28 @@
 const { exec } = require("child_process");
-const log = require('electron-log');
-const path = require('path');
+const log = require("electron-log");
+const path = require("path");
+var os = require("os");
 
 const runCommand = async (command) => {
   return new Promise((resolve, reject) => {
-    const pathToHwi = path.resolve(__dirname, 'HWI'); // for mac, change when building
+    let hwiFile = "HWI.exe";
+
+    console.log(os.platform());
+
+    if (os.platform() === "linux") hwiFile = "HWI_LINUX";
+    if (os.platform() === "darwin") hwiFile = "HWI_MAC";
+
+    // const pathToHwi = path.resolve(__dirname, 'HWI'); // for mac, change when building
     // const pathToHwi = path.resolve(__dirname, 'hwi.exe'); // for windows, change when building
+    const pathToHwi = path.resolve(__dirname, hwiFile); // for mac, change when building
     exec(`${pathToHwi} ${command}`, (error, stdout, stderr) => {
       if (error) {
-        log.info('error: ', error);
+        log.info("error: ", error);
         reject(error);
       }
       resolve(stdout);
     });
   });
-}
+};
 
 module.exports = runCommand;
