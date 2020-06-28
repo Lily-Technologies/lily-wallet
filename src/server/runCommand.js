0afd067136075bf791c5757533bf0aef03ae0e2e
Leonardo <leo.jobim@gmail.com>
2020-06-28 09:12:02 -0300

    Adds hwi for Windows and replace exec command by execFile to avoid error when there's blank space in hwi file path

--- src/server/runCommand.js
+++ src/server/runCommand.js
@@ -1,21 +1,17 @@
-const { exec } = require("child_process");
+const { execFile } = require("child_process");
 const log = require("electron-log");
 const path = require("path");
 var os = require("os");
 
 const runCommand = async (command) => {
   return new Promise((resolve, reject) => {
-    let hwiFile = "HWI.exe";
-
-    console.log(os.platform());
+    let hwiFile = "hwi.exe";
 
     if (os.platform() === "linux") hwiFile = "HWI_LINUX";
     if (os.platform() === "darwin") hwiFile = "HWI_MAC";
 
-    // const pathToHwi = path.resolve(__dirname, 'HWI'); // for mac, change when building
-    // const pathToHwi = path.resolve(__dirname, 'hwi.exe'); // for windows, change when building
-    const pathToHwi = path.resolve(__dirname, hwiFile); // for mac, change when building
-    exec(`${pathToHwi} ${command}`, (error, stdout, stderr) => {
+    const pathToHwi = path.resolve(__dirname, hwiFile);
+    execFile(pathToHwi, [command], (error, stdout, stderr) => {
       if (error) {
         log.info("error: ", error);
         reject(error);
