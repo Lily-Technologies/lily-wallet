63421d89190e869799da53d1d9316e00eab27dfd
Kevin Mulcrone <kaybesee@gmail.com>
2021-02-05 19:54:16 -0700

    add test for license.ts

---
+++ src/__tests__/utils/license.test.ts
@@ -0,0 +1,18 @@
+import { isAtLeastTier } from "../../utils/license";
+
+const exampleLicense = {
+  signature:
+    "KPP1aFqppOLQAvfusPpw2+mED6sE4qF1RuZcS/vd67fycd/j7PSNcUGnUEn9ABNEtdXV9+XlNtiqBsuAYghBYjw=",
+  license:
+    "basic:721676:a1b6131ba536d9994afb0bac01936869a2d2f1d59eecb2d588727c4ab1e47dee",
+};
+
+describe("license.ts", () => {
+  test("isAtLeastTier: ", () => {
+    expect(isAtLeastTier(exampleLicense, "trial")).toBe(false);
+    expect(isAtLeastTier(exampleLicense, "premium")).toBe(false);
+    expect(isAtLeastTier(exampleLicense, "essential")).toBe(false);
+    expect(isAtLeastTier(exampleLicense, "basic")).toBe(true);
+    expect(isAtLeastTier(exampleLicense, "free")).toBe(true);
+  });
+});
