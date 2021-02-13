f69f60e5e0578dbd731938ecd11e36dd3dabecd8
Kevin Mulcrone <kaybesee@gmail.com>
2021-02-12 18:42:50 -0700

    add fonts, remove google fonts

--- src/pages/Setup/PageHeader.tsx
+++ src/pages/Setup/PageHeader.tsx
@@ -1,17 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { HeaderWrapper, CancelButton, PageTitleSubtext } from './styles';
-import { Header, HeaderLeft, HeaderRight, PageTitle, Dropdown } from '../../components/';
+import { HeaderWrapper, CancelButton, PageTitleSubtext } from "./styles";
+import {
+  Header,
+  HeaderLeft,
+  HeaderRight,
+  PageTitle,
+  Dropdown,
+} from "../../components/";
 
-import { black } from '../../utils/colors';
+import { black } from "../../utils/colors";
 
-import { SetStateNumber } from '../../types';
+import { SetStateNumber } from "../../types";
 
 interface Props {
-  headerText: string
-  setStep: SetStateNumber
-  step: number
-  setSetupOption: SetStateNumber
+  headerText: string;
+  setStep: SetStateNumber;
+  step: number;
+  setSetupOption: SetStateNumber;
 }
 
 const PageHeader = ({ headerText, setStep, step, setSetupOption }: Props) => {
@@ -22,22 +28,40 @@ const PageHeader = ({ headerText, setStep, step, setSetupOption }: Props) => {
       <Header>
         <HeaderLeft>
           <PageTitleSubtext>New Account</PageTitleSubtext>
-          <PageTitle style={{ color: black }}>{headerText}</PageTitle>
+          <PageTitle style={{ color: black, fontWeight: 500 }}>
+            {headerText}
+          </PageTitle>
         </HeaderLeft>
         <HeaderRight>
-          {step === 0 && <Dropdown
-            isOpen={dropdownOpen}
-            setIsOpen={setDropdownOpen}
-            minimal={true}
-            dropdownItems={[
-              { label: 'New Software Wallet', onClick: () => { setSetupOption(2); setStep(1) } },
-            ]}
-          />}
-          {step > 0 && <CancelButton onClick={() => { setStep(0) }}>Cancel</CancelButton>}
+          {step === 0 && (
+            <Dropdown
+              isOpen={dropdownOpen}
+              setIsOpen={setDropdownOpen}
+              minimal={true}
+              dropdownItems={[
+                {
+                  label: "New Software Wallet",
+                  onClick: () => {
+                    setSetupOption(2);
+                    setStep(1);
+                  },
+                },
+              ]}
+            />
+          )}
+          {step > 0 && (
+            <CancelButton
+              onClick={() => {
+                setStep(0);
+              }}
+            >
+              Cancel
+            </CancelButton>
+          )}
         </HeaderRight>
       </Header>
     </HeaderWrapper>
-  )
-}
+  );
+};
 
 export default PageHeader;
