eefcc9dd42850e44c86eacf5b89ef50a16ca564b
Kevin Mulcrone <kaybesee@gmail.com>
2021-04-23 16:57:06 -0600

    up to 1.0.3

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,13 +1,31 @@
-import { css } from 'styled-components';
+import { css } from "styled-components";
 
-export const mobile = inner => css`
+export const mobile = (inner) => css`
   @media (max-width: ${1000 / 16}em) {
     ${inner};
   }
 `;
 
-export const phone = inner => css`
+export const phone = (inner) => css`
   @media (max-width: ${650 / 16}em) {
+    ${inner};
+  }
+`;
+
+export const sm = (inner) => css`
+  @media (min-width: 640px) {
+    ${inner};
+  }
+`;
+
+export const md = (inner) => css`
+  @media (min-width: 768px) {
+    ${inner};
+  }
+`;
+
+export const lg = (inner) => css`
+  @media (min-width: 1024px) {
     ${inner};
   }
 `;
