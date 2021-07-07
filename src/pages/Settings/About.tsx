bb459c51e888459c9d3e4e981cc2c47eae16a8d6
Kevin Mulcrone <kaybesee@gmail.com>
2021-07-07 13:01:26 -0600

    pull version number from package.json

--- src/pages/Settings/About.tsx
+++ src/pages/Settings/About.tsx
@@ -28,7 +28,7 @@ const About = () => {
               paddingRight: "1.5rem",
             }}
           >
-            {config.version}
+            {process.env.REACT_APP_VERSION}
           </SettingsTable.ValueText>
         </SettingsTable.ValueColumn>
       </SettingsTable.Row>
