939f19d59f0ec05b4ce3bd0bc301d0d851c0fab1
Jordan Brookman <Jordan.Brookman@ibm.com>
2022-10-12 15:03:37 -0400

    Add library for React DevTools

--- apps/electron/src/main.ts
+++ apps/electron/src/main.ts
@@ -37,6 +37,14 @@ import {
   OnchainProviderConnectionDetails
 } from '@lily/types';
 
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+
+app.whenReady().then(() => {
+    installExtension(REACT_DEVELOPER_TOOLS)
+        .then((name) => console.log(`Added Extension:  ${name}`))
+        .catch((err) => console.log('An error occurred: ', err));
+});
+
 const PROTOCOL_PREFIX = 'lily';
 
 // disable showErrorBox
