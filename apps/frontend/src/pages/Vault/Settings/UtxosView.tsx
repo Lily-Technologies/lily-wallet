a33a3873013553fabf368b1689ebe45c73c9f8b2
Kevin Mulcrone <KayBeSee@gmail.com>
2022-10-20 22:25:11 -0600

    feat(UTXORow): add search, tags (#102)
    see lily-technologies/lily-wallet#59

--- apps/frontend/src/pages/Vault/Settings/UtxosView.tsx
+++ apps/frontend/src/pages/Vault/Settings/UtxosView.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { requireOnchain } from 'src/hocs';
 
 import { SettingsTable } from 'src/components';
+import { SearchToolbar } from 'src/pages/Send/components/SelectInputsForm/SearchToolbar';
 
 import { LilyOnchainAccount } from '@lily/types';
 
@@ -13,7 +14,10 @@ interface Props {
 }
 
 const UtxosView = ({ currentAccount }: Props) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [showTags, setShowTags] = useState(false);
   const { availableUtxos } = currentAccount;
+
   return (
     <>
       <SettingsTable.HeaderSection>
@@ -23,15 +27,24 @@ const UtxosView = ({ currentAccount }: Props) => {
         </SettingsTable.HeaderSubtitle>
       </SettingsTable.HeaderSection>
       <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-        <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
+        <SearchToolbar
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          setShowTags={setShowTags}
+          showTags={showTags}
+        />
+        <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 bg-gray-50 dark:bg-slate-900 border border-slate-600/20 mb-8'>
           <div className='overflow-hidden sm:rounded-lg'>
-            <table className='border-collapse w-full'>
-              <tbody className='divide-y dark:divide-slate-400/10'>
-                {availableUtxos.map((utxo) => (
-                  <UtxoRow key={utxo.txid} utxo={utxo} />
-                ))}
-              </tbody>
-            </table>
+            <ul className='space-y-4 py-4'>
+              {availableUtxos.map((utxo) => (
+                <UtxoRow
+                  key={utxo.txid}
+                  utxo={utxo}
+                  searchQuery={searchQuery}
+                  showTags={showTags}
+                />
+              ))}
+            </ul>
           </div>
         </div>
       </div>
