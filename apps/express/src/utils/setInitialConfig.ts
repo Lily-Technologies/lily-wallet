71ed1e89c73b0a512ee79bb0dcde5df10f9edee7
Kevin Mulcrone <KayBeSee@gmail.com>
2022-10-23 14:41:03 -0600

    feat(Express): MyNode integration
    adds some logic to allow initial config file to be saved while letting user set password
    
    see lily-technologies/lily-wallet#93

--- apps/express/src/utils/setInitialConfig.ts
+++ apps/express/src/utils/setInitialConfig.ts
@@ -17,7 +17,8 @@ export const setInitialConfig = async () => {
     console.log('No config file exists');
   }
 
-  if (!configExists && process.env.APP_PASSWORD) {
+  // assuming if LND_WALLET_NAME is an env variable, then setup wallet
+  if (!configExists && process.env.LND_WALLET_NAME) {
     try {
       const { file: macaroon } = await getFile(
         'admin.macaroon',
@@ -28,7 +29,7 @@ export const setInitialConfig = async () => {
       const lndHost = `${process.env.LND_IP}:${process.env.LND_GRPC_PORT}`;
 
       const emptyConfig = EMPTY_CONFIG;
-      emptyConfig.isEmpty = false;
+      emptyConfig.isEmpty = true;
       emptyConfig.lightning[0] = {
         id: uuidv4(),
         type: 'lightning',
@@ -44,13 +45,18 @@ export const setInitialConfig = async () => {
         }
       };
 
-      const encryptedConfigObject = AES.encrypt(
-        JSON.stringify(emptyConfig),
-        process.env.APP_PASSWORD
-      ).toString();
-
-      await saveFile(encryptedConfigObject, CONFIG_FILE_NAME, APP_DATA_DIRECTORY);
-      console.log(`Saved ${CONFIG_FILE_NAME} to ${APP_DATA_DIRECTORY}`);
+      if (!configExists && process.env.APP_PASSWORD) {
+        const encryptedConfigObject = AES.encrypt(
+          JSON.stringify(emptyConfig),
+          process.env.APP_PASSWORD
+        ).toString();
+        await saveFile(encryptedConfigObject, CONFIG_FILE_NAME, APP_DATA_DIRECTORY);
+        console.log(`Saved ${CONFIG_FILE_NAME} to ${APP_DATA_DIRECTORY}`);
+      } else {
+        const unencryptedConfig = JSON.stringify(emptyConfig);
+        await saveFile(unencryptedConfig, CONFIG_FILE_NAME, APP_DATA_DIRECTORY);
+        console.log(`Saved ${CONFIG_FILE_NAME} to ${APP_DATA_DIRECTORY} (unencrypted)`);
+      }
     } catch (e) {
       console.log('error: ', e);
     }
